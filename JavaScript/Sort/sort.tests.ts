688e1753cbe1a89b93944a4dc1bfb430af50c1fc
orvinjedalava <ojedbub@gmail.com>
2024-08-02 21:57:04 +1000

    Adding insertionSort implementation

--- JavaScript/Sort/sort.tests.ts
+++ JavaScript/Sort/sort.tests.ts
@@ -1,7 +1,10 @@
-import { selectionSort } from './sort';
+import { selectionSort, insertionSort } from './sort';
 
-describe('testing selectionSort function', () => {
-    test('resulting array is not equal to expected array', () => {
+describe('testing sort functions', () => {
+    test('selectionSort: resulting array is equal to expected array', () => {
         expect(selectionSort([5,1,4,0,2,3])).toEqual([0,1,2,3,4,5]);
+    });
+    test('insertionSort: resulting array is equal to expected array', () => {
+        expect(insertionSort([5,1,4,0,2,3])).toEqual([0,1,2,3,4,5]);
     });
 });
