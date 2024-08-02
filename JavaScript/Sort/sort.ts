8daa6861cf41705a8d2456d44182e02a998c838e
orvinjedalava <ojedbub@gmail.com>
2024-08-02 21:30:47 +1000

    Adding selectionSort implementation

--- JavaScript/Sort/sort.ts
+++ JavaScript/Sort/sort.ts
@@ -1,3 +1,17 @@
 export function selectionSort(arr: number[]): number[] {
+
+    for (let i = 0; i < arr.length - 2; i++) {
+        let minIdx = i;
+
+        for(let j = i + 1; j < arr.length; j++) {
+            if (arr[minIdx] > arr[j]) {
+                minIdx = j;
+            }
+        }
+
+        let temp = arr[i];
+        arr[i] = arr[minIdx];
+        arr[minIdx] = temp;
+    }
     return arr;
 }
