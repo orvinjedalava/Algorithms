ae0616b721872d21f66078b4ed15a077f5460a20
orvinjedalava <ojedbub@gmail.com>
2024-08-02 18:00:36 +1000

    Initial commit to add jest for unit testing

---
+++ JavaScript/Sort/sort.tests.ts
@@ -0,0 +1,7 @@
+import { selectionSort } from './sort';
+
+describe('testing selectionSort function', () => {
+    test('resulting array is not equal to expected array', () => {
+        expect(selectionSort([5,1,4,0,2,3])).toEqual([5,1,4,0,2,3]);
+    });
+});
