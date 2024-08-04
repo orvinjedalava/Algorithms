58503c9fe797ccf60ba8635f2b90118f47d8a9c2
orvinjedalava <ojedbub@gmail.com>
2024-08-04 22:45:11 +1000

    Adding Hospital and Candidate classes

--- JavaScript/StableMatching/myObjects.ts
+++ JavaScript/StableMatching/myObjects.ts
@@ -1,3 +1,18 @@
 class Hospital {
+    constructor(name: string, preferredCandidates: string[]) {
+        this.name = name;
+        this.preferredCandidates = preferredCandidates;
+    }
+    name: string ;
+    preferredCandidates: string[];
+}
+
+class Candidate {
+    constructor(name: string, preferredHospitals: {[key: string]: number}) {
+        this.name = name;
+        this.preferredHospitals = preferredHospitals;
+    }
+
     name: string = '';
+    preferredHospitals: {[key: string]: number} = {}
 }
