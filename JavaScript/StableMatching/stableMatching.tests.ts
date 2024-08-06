95c6e6fb866bd350bd7a4f520219d8891a4cbb04
orvinjedalava <ojedbub@gmail.com>
2024-08-06 22:31:31 +1000

    Updating StableMatching test script

--- JavaScript/StableMatching/stableMatching.tests.ts
+++ JavaScript/StableMatching/stableMatching.tests.ts
@@ -3,10 +3,10 @@ import { Hospital, Candidate } from './myObjects';
 
 describe('Testing stable matching functions', () => {
     test('galeShapley: resulting array is equal to expected array',() => {
-        let hospitals: Hospital[] = [];
-        let candidates: Candidate[] = [];
-
+        let hospitals: Hospital[] = [ new Hospital('A-H', [ 'A-C', 'B-C']), new Hospital('B-H', [ 'A-C', 'B-C'])];
+        let candidates: Candidate[] = [ new Candidate('A-C', {'A-H': 1, 'B-H': 2})];
         let expectedResult: {[key: string]: string} = { 'A-C': 'A-H', 'B-C': 'B-H' };
+        
         expect(galeShapley(hospitals, candidates)).toEqual(expectedResult);
     });
 });
