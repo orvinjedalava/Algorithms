e12727da171c1c0ada1768b8a325a00d5e309488
orvinjedalava <ojedbub@gmail.com>
2024-08-12 09:24:08 +1000

    Adding implementation of InsertionSort using GitHub Copilot

--- CSharp/Sort/SortService.cs
+++ CSharp/Sort/SortService.cs
@@ -6,7 +6,23 @@ namespace Sort
     {
         public int[] InsertionSort(int[] array)
         {
-            throw new NotImplementedException();
+            for (int i = 1; i < array.Length; i++)
+            {
+                int key = array[i];
+                int j = i - 1;
+
+                //modify
+
+                // Move elements of array[0..i-1], that are greater than key,
+                // to one position ahead of their current position
+                while (j >= 0 && array[j] > key)
+                {
+                    array[j + 1] = array[j];
+                    j = j - 1;
+                }
+                array[j + 1] = key;
+            }
+            return array; 
         }
             
     }
