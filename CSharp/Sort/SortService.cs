9205c4540d0347666c0cc9d8f0df1aadefe70768
orvinjedalava <ojedbub@gmail.com>
2024-08-12 09:28:00 +1000

    Further updates

--- CSharp/Sort/SortService.cs
+++ CSharp/Sort/SortService.cs
@@ -11,8 +11,6 @@ namespace Sort
                 int key = array[i];
                 int j = i - 1;
 
-                //modify
-
                 // Move elements of array[0..i-1], that are greater than key,
                 // to one position ahead of their current position
                 while (j >= 0 && array[j] > key)
